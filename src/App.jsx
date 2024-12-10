f3353e495e840da67cb58ef586496593841f5601
PedroVViana <pedro.vviana@hotmail.com>
2024-12-09 23:25:09 -0300

    change SEO files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import SEO from "./components/SEO";
 import Article from "./components/Article";
 import "./App.css";
 import Footer from "./components/Footer";
@@ -7,11 +6,6 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <div>
-      <SEO
-        title="Java vs. JavaScript: Entenda as Diferenças de Forma Simples"
-        description="Descubra as principais diferenças entre Java e JavaScript e saiba qual linguagem escolher para os seus projetos."
-        keywords="Java, JavaScript, programação, linguagens de programação, diferenças entre Java e JavaScript"
-      />
       <Article />
       <Footer/>
     </div>
